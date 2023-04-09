ddf498c91f07158978ec474dfb0818bd48a7e508
user <user@gmail.com>
2023-04-09 20:36:19 +0900

    最大幅設定

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { PasswordGenerator } from "./components/PasswordGenerator"
 
 function App() {
   return (
-    <div className="touch-none select-none p-[1rem] ">
+    <div className="touch-none select-none p-[1rem] max-w-[430px] mx-auto">
       <PasswordGenerator />
       <Footer />
     </div>
