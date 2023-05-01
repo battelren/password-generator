15aec0c49d6aff5edc57c39cad4ecc31d0e9de42
user <user@gmail.com>
2023-05-01 23:16:19 +0900

    フォントサイズ修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const repoUrl = "https://github.com/battelren/password-generator"
 
 const Footer: React.FC = () => {
   return (
-    <div className="flex flex-col gap-y-[1rem] justify-center text-gray-500 text-sm font-medium mx-[1.5rem]">
+    <div className="flex flex-col gap-y-[1rem] justify-center text-gray-500 text-[0.8rem] font-medium mx-[1.5rem]">
       <div>
         <div className="font-bold">■開発言語など</div>
         <div>React, TypeScript, Heroicons</div>
